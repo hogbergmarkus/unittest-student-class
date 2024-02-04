a61629c3a80514479efed7176b090b314b0eaf23
Markus Högberg <m.o.hogberg@gmail.com>
2024-02-04 14:51:11 +0000

    Refactor test code

--- test_student.py
+++ test_student.py
@@ -3,23 +3,43 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
-
-        self.assertEqual(student.full_name, 'John Doe')
+    # @classmethod
+    # def setUpClass(cls):
+    #     print('setUpClass')
     
 
-    def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
+    # @classmethod
+    # def tearDownClass(cls):
+    #     print('tearDownClass')
 
-        self.assertTrue(student.naughty_list)
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+
+    def tearDown(self):
+        print('tearDown')
+
+
+    def test_full_name(self):
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
     
 
     def test_email(self):
-        student = Student('John', 'Doe')
+        print('test_email')
+        self.student = Student('John', 'Doe')
 
-        self.assertEqual(student.email, 'john.doe@email.com')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
+    
+
+    def test_alert_santa(self):
+        print('test_alert_santa')
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naughty_list)
+
 
 
 if __name__ == '__main__':
